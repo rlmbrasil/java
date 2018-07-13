8bef22818e1e5a4a0dba7bf6d320b923072f44cf
Regis Moraes <rlmbrasil@icloud.com>
2018-07-13 18:08:23 -0300

    Training OlaMundo

---
+++ Aula 05/Regis.java
@@ -0,0 +1,6 @@
+class Regis{
+
+	public static void main(String[] args) {
+		System.out.println("SOU REGIS MORAES, MUITO PRAZER");
+		}	
+	}
---
+++ Aula 05/ThalesBueno.java
@@ -0,0 +1,8 @@
+class ThalesBueno {
+	
+	public static void main (String[] args){
+		
+		System.out.println("Olá, sou o Thales Bueno");
+
+	}
+}
